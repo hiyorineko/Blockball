2fa9f614ce35ae560b2c4506950148c8b59387fb
hiyoriaya <hiyoriaya@gmail.com>
2016-01-19 23:32:17 +0900

    project start

---
+++ app/src/main/java/com/example/hiyoriaya/blockball/GameView.java
@@ -0,0 +1,18 @@
+package com.example.hiyoriaya.blockball;
+
+import android.content.Context;
+import android.view.TextureView;
+
+/**
+ * Created by hiyoriaya on 2016/01/19.
+ * ゲーム画面描写用のTextureView
+ */
+public class GameView extends TextureView{
+
+    public GameView(Context context){
+        super(context); //親クラスからcontextを受ける
+    }
+
+
+
+}
